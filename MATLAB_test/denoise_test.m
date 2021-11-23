6676eb38894e15f49e860233530c0252b15a4478
chadraymor <77703531+chadraymor@users.noreply.github.com>
2021-11-22 22:32:12 -0600

    test 2

--- MATLAB_test/denoise_test.m
+++ MATLAB_test/denoise_test.m
@@ -1,13 +1,13 @@
 original_Image = imread('ctscan.jpg')
 
-if size(original_Image, 3) == 3
+if size(original_Image, 3) == 3 %3 refers to color channels(rgb). this converts image to grayscale if its rgb
     original_Image = rgb2gray(original_Image);
 end
 
-noisy_Image =  imnoise(original_Image, 'gaussian', 0, 0.03);
+noisy_Image =  imnoise(original_Image, 'gaussian', 0, 0.03);%.03 refers to amount of noise added
 
-net = denoisingNetwork('DnCNN');
+net = denoisingNetwork('DnCNN');% denoise CNN network
 denoised_Image = denoiseImage(noisy_Image, net);
 
 montage({original_Image, noisy_Image, denoised_Image})
-title("denoised")
+title("original, with noise, denoised")
